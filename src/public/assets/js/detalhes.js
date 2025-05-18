07d8cae3ba5a0b190b7d763457839d9d744aa7ac
guilhermetft <guiftf2007@gmail.com>
2025-05-18 14:44:46 -0300

    id da página de detalhes

--- src/public/assets/js/detalhes.js
+++ src/public/assets/js/detalhes.js
@@ -6,12 +6,16 @@ function getParameter(name) {
 
 function carregarDetalhes() {
     const refeicaoId = getUrlParameter('id');
+
+
+    fetch('db/refeicoes.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao carregar os dados');
+            }
+            return response.json();
+        })
 }
 
-fetch('db/refeicoes.json')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Erro ao carregar os dados');
-        }
-        return response.json();
-    })
+document.addEventListener
+('DOMContentLoaded', carregarDetalhes);
