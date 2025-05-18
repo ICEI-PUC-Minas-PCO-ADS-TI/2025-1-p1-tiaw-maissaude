ce48ae358e35231584af18fe4223860a2807a487
guilhermetft <guiftf2007@gmail.com>
2025-05-18 14:40:59 -0300

    js da página de detalhes

---
+++ src/public/assets/js/detalhes.js
@@ -0,0 +1,17 @@
+function getParameter(name) {
+    const urlParams = new URLSearchParams
+        (window.location.search);
+    return urlParams.get(name);
+}
+
+function carregarDetalhes() {
+    const refeicaoId = getUrlParameter('id');
+}
+
+fetch('db/refeicoes.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao carregar os dados');
+        }
+        return response.json();
+    })
