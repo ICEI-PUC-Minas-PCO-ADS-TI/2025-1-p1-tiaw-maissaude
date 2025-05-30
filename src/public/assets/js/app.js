e2d361b74f0071e81503f051408d4f1f4d5145bb
Gustavo Silva Santiago <gustavox00.santi@outlook.com>
2025-05-30 15:40:06 -0300

    Adicionando as imagens e fazendo as alterações necessarias

--- src/public/assets/js/app.js
+++ src/public/assets/js/app.js
@@ -3,36 +3,36 @@
 const featureData = [
     [
         {
-            img: "img/nutricao1.png",
+            img: "assets/img/nutricao1.png",
             title: "Acompanhamento inteligente",
             description: "Acompanhe de forma simples e rápida sua saúde, nosso site nutricional"
         },
         {
-            img: "img/imagemFeatures4.webp",
+            img: "assets/img/imagemFeatures4.webp",
             title: "Monitore sua saúde facilmente",
             description: "Acompanhe indicadores como IMC, ingestão de nutrientes e hábitos alimentares em tempo real."
         }
     ],
     [
         {
-            img: "img/nutricao2.jpg",
+            img: "assets/img/nutricao2.jpg",
             title: "Mais cores no prato,<br>mais saúde na vida.",
             description: "A alimentação saudável é a chave para uma vida mais proativa e equilibrada, além de prevenir diversas doenças e deficiências físicas, monitore a sua saúde agora mesmo com o Ma+s Saude."
         },
         {
-            img: "img/imagemFeatures5.jpg",
+            img: "assets/img/imagemFeatures5.jpg",
             title: "Descubra novos sabores naturais",
             description: "Combine vegetais, frutas e leguminosas para criar pratos deliciosos e nutritivos."
         }
     ],
     [
         {
-            img: "img/nutricao3.jpg",
+            img: "assets/img/nutricao3.jpg",
             title: "<span class='highlight'>Atenção</span> Perca de forma tradicional",
             description: "Cada prato \"feita de forma tradicional\" é uma ode à excelência. Desde o meticuloso corte do sashimi até a preparação delicada do sushi, cada etapa é uma reverência à herança culinária do Japão."
         },
         {
-            img: "img/ImagemFeatures6.jpg",
+            img: "assets/img/ImagemFeatures6.jpg",
             title: "<span class='highlight'>Dica</span> Herança e sabor na medida certa",
             description: "Adote receitas milenares adaptadas à nutrição moderna com o toque do Ma+s Saúde."
         }
